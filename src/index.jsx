d64355999041e36a88ac812dd5bfc7f8034343c0
Deshi Rahim <nawal.rahim@gmail.com>
2022-11-20 11:06:23 -0800

    👽 Update create createRoot api

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-ReactDOM.render(
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
