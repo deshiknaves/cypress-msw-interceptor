f4fa7f1b568e670ac5e2c09e5ec7e30d436ed77b
Deshi Rahim <nawal.rahim@gmail.com>
2022-11-20 11:31:29 -0800

    👽 Upgarde for cypress 11

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: false,
+  projectId: 'jcb2wm',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
